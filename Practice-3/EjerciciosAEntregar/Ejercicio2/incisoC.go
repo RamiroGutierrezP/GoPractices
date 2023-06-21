1b073d2f7ffa8d7314c7ac2f0ea53c4ef4e9d726
Ramiro Gutierrez <ramagp00@gmail.com>
2023-06-21 17:46:25 -0300

    m

--- Practice-3/EjerciciosAEntregar/Ejercicio2/incisoC.go
+++ Practice-3/EjerciciosAEntregar/Ejercicio2/incisoC.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	wg := sync.WaitGroup{}
-	colas := crearCanales(3)
+	colas := crearColasConBuffer(3)
 	cajas := crearCanales(3)
 	go clientesLlegando(colas)
 	wg.Add(3)
@@ -24,15 +24,32 @@ func main() {
 	}
 	wg.Wait()
 }
-
+func crearColasConBuffer(cantidad int) []chan int{
+	colas := make([]chan int, cantidad)
+	for i := 0; i < cantidad; i++ {
+		colas[i] = make(chan int, 7)
+	}
+	return colas
+}
 func clientesLlegando(colas []chan int) {
-	for i := 0; i < 22; i++ {
-		//Envío el cliente a la cola correspondiente mediante el algoritmo de round robin
-		colas[i % 3] <- i
+	for i := 0; i < 5; i++ {
+		//Envío el cliente a la cola que menos clientes tenga
+		obtenerColaConMenosClientes(colas) <- i		
 	}
 	for i := 0; i < 3; i++ {
 		close(colas[i])
 	}
+}
+func obtenerColaConMenosClientes(colas []chan int) chan int{
+	min := len(colas[0])
+	minIndex := 0
+	for i := 1; i < len(colas); i++ {
+		if len(colas[i]) < min {
+			min = len(colas[i])
+			minIndex = i
+		}
+	}
+	return colas[minIndex]
 }
 func crearCanales(cantidad int) []chan int{
 	canales := make([]chan int, cantidad)
--- Practice-3/EjerciciosAEntregar/Ejercicio2/main.go
+++ Practice-3/EjerciciosAEntregar/Ejercicio2/main.go
@@ -9,30 +9,35 @@ import (
 
 func main() {
 	wg := sync.WaitGroup{}
-	colas := crearCanales(3)
-	cajas := crearCanales(3)
+	colas := crearColasConBuffer(3)
 	go clientesLlegando(colas)
-	wg.Add(3)
-	for i := 0; i < 3; i++ {
-		go func(i int) {
-			for cliente := range colas[i] {
-				go atenderCliente(cliente, i, cajas[i])
-				<-cajas[i]
-			}
-			wg.Done()
-		}(i)
-	}
-	wg.Wait()
-}
 
-func clientesLlegando(colas []chan int) {
-	for i := 0; i < 22; i++ {
-		//Envío el cliente a la cola correspondiente mediante el algoritmo de round robin
-		colas[i % 3] <- i
+	for i := 0; i < 5; i++ {
+		//Envío el cliente a la cola que menos clientes tenga
+		obtenerColaConMenosClientes(colas) <- i		
 	}
 	for i := 0; i < 3; i++ {
 		close(colas[i])
 	}
+}
+func crearColasConBuffer(cantidad int) []chan int{
+	colas := make([]chan int, cantidad)
+	for i := 0; i < cantidad; i++ {
+		colas[i] = make(chan int, 7)
+	}
+	return colas
+}
+
+func obtenerColaConMenosClientes(colas []chan int) chan int{
+	min := len(colas[0])
+	minIndex := 0
+	for i := 1; i < len(colas); i++ {
+		if len(colas[i]) < min {
+			min = len(colas[i])
+			minIndex = i
+		}
+	}
+	return colas[minIndex]
 }
 func crearCanales(cantidad int) []chan int{
 	canales := make([]chan int, cantidad)
